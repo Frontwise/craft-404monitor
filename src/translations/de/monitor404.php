793696061de47b356fe6a1d85a5ed90891960531
Simon D. Müller <simon.d.mueller@gmail.com>
2018-10-04 23:13:25 +0000

    added german translation

---
+++ src/translations/de/monitor404.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Are you sure you want to delete all web 404 requests?' => 'Bist du sicher, dass du alle 404 Requests löschen möchtest?',
+    'Delete all requests' => 'Alle Requests löschen',
+    'Delete' => 'Löschen',
+    'All 404 requests' => 'Alle 404 Requests',
+    'Are you sure you want to delete the selected 404 request?' => 'Bist du sicher das du die ausgewählten 404 Requests löschen möchtest?',
+    'Request deleted.' => 'Request(s) gelöscht.',
+    'Date Created' => 'Erstellt am',
+    'URL' => 'URL',
+    'IP Address' => 'IP Adresse',
+    'User Agent' => 'User Agent',
+    'Message' => 'Bericht',
+    'File' => 'Datei',
+    'Hits' => 'Aufrufe',
+    'Last hit' => 'Letzter Aufruf',
+];
